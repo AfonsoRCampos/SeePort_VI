ec0269b0e08c37ac0f4f6f144d6352097d78d010
AfonsoCampos971 <afonso.campos@ua.pt>
2024-11-19 22:18:39 +0000

    fixes

--- temp.py
+++ temp.py
@@ -14,24 +14,33 @@ def calculate_movement_counts(data, start, end, date_range, mean_of_transport=No
     if in_out:
         filtered_data = filtered_data[(filtered_data['In/Out'] == in_out)]
     display_data = pd.DataFrame(date_range, columns=['Date'])
-    for i in range(len(display_data)):
+    for i in range(len(display_data) - 1):
         date = display_data["Date"][i]
-        if i == len(display_data) - 1:
-            next_date = end
-        else:
-            next_date = display_data["Date"][i + 1]
-        if next_date.date() == date.date(): continue
-        period_data = filtered_data[(filtered_data["Date"] >= date) & (filtered_data["Date"] < next_date)]
+
+        next_date = display_data["Date"][i + 1]
         
-        count = len(period_data)
-        count_in = len(period_data[(filtered_data["In/Out"] == "In")])
-        count_out = len(period_data[(filtered_data["In/Out"] == "Out")])
+        count = len(filtered_data[(filtered_data["Date"] >= date) & (filtered_data["Date"] < next_date)])
+        count_in = len(filtered_data[(filtered_data["Date"] >= date) & (filtered_data["Date"] < next_date) & (filtered_data["In/Out"] == "In")])
+        count_out = len(filtered_data[(filtered_data["Date"] >= date) & (filtered_data["Date"] < next_date) & (filtered_data["In/Out"] == "Out")])
         display_data.loc[i, "Count"] = int(count)
         display_data.loc[i, "In"] = int(count_in)
         display_data.loc[i, "Out"] = int(count_out)
         display_data.loc[i, "Period Start"] = date
         display_data.loc[i, "Period End"] = next_date
+        
     return display_data, filtered_data
+
+def round_out_start_end(start, end, granularidade):
+    if granularidade == "Dia" or granularidade == "Hora":
+        start = pd.to_datetime(start).replace(hour=0, minute=0, second=0)
+        end = (pd.to_datetime(end) + pd.Timedelta(days=1)).replace(hour=0, minute=0, second=0)
+    elif granularidade == "Semana":
+        start = pd.to_datetime(start).to_period('W').start_time
+        end = pd.to_datetime(end).to_period('W').end_time.replace(hour=0, minute=0, second=0)
+    elif granularidade == "Mês":
+        start = pd.to_datetime(start).replace(day=1, hour=0, minute=0, second=0)
+        end = (pd.to_datetime(end) + pd.offsets.MonthBegin(1)).replace(hour=0, minute=0, second=0)
+    return start, end
 
 # Carregar os dados iniciais
 data = pd.read_csv("data/container_throughput.csv", parse_dates=['Date'])
@@ -49,31 +58,36 @@ st.title("Dashboard de Contentores")
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    granularidade = st.radio("Granularidade:", ["Hora", "Dia", "Semana", "Mês"], index=3)
+    granularidade = st.radio("Agrupar os dados por:", ["Hora", "Dia", "Semana", "Mês"], index=3)
 
-gran_freq_dict = {"Hora": "h", "Dia": "D", "Semana": "W", "Mês": "M"}
+gran_freq_dict = {"Hora": "h", "Dia": "D", "Semana": "W", "Mês": "MS"}
 gran_format_dict = {"Hora": "%H:%M", "Dia": "%d/%m/%Y", "Semana": "%d/%m/%Y", "Mês": "%m/%Y"}
 
 with col2:
     date_slider = st.checkbox("Usar slider para datas", True)
 
 if date_slider:
-    start, end = st.slider("Periodo a visualizar:", min_value=min_date, max_value=max_date, value=(pd.to_datetime("2022/01/01").date(), pd.to_datetime("2023/01/01").date()), format="DD/MM/YYYY")
+    dates = st.slider(
+        "Periodo a visualizar:", 
+        min_value=min_date, 
+        max_value=max_date, 
+        value=(start, end), 
+        format="DD/MM/YYYY"
+    )
 else:
-    start, end = st.date_input("Periodo a visualizar:", min_value=min_date, max_value=max_date, value=(pd.to_datetime("2022/01/01").date(), pd.to_datetime("2023/01/01").date()), format="DD/MM/YYYY")
+    dates = st.date_input(
+        "Periodo a visualizar:", 
+        min_value=min_date, 
+        max_value=max_date, 
+        value=(start, end)
+    )
 
-#testar
-def round_out_start_end(start, end, granularidade):
-    if granularidade == "Dia":
-        start = pd.to_datetime(start).replace(hour=0, minute=0, second=0)
-        end = pd.to_datetime(end).replace(hour=23, minute=59, second=59)
-    elif granularidade == "Semana":
-        start = pd.to_datetime(start).replace(hour=0, minute=0, second=0) - pd.DateOffset(days=pd.to_datetime(start).weekday())
-        end = pd.to_datetime(end).replace(hour=23, minute=59, second=59) + pd.DateOffset(days=6 - pd.to_datetime(end).weekday())
-    elif granularidade == "Mês":
-        start = pd.to_datetime(start).replace(day=1, hour=0, minute=0, second=0)
-        end = pd.to_datetime(end).replace(day=pd.to_datetime(end).days_in_month, hour=23, minute=59, second=59)
-    return start, end
+# Validar se ambos os valores de data estão definidos
+if isinstance(dates, tuple) and len(dates) == 2:
+    start, end = dates
+else:
+    st.warning("Por favor, selecione um intervalo completo de datas.")
+    st.stop()
 
 start, end = round_out_start_end(start, end, granularidade)
     
@@ -81,13 +95,7 @@ brush = alt.selection_interval(encodings=['x'])
 
 bar_select = alt.selection_point(fields=['Date', 'Period Start', 'Period End'], nearest=True, empty='none', on='click')
 
-with st.expander("Movimentação de Contentores", expanded=False):
-    date_range = pd.date_range(start=start, end=end, freq=gran_freq_dict[granularidade])
-
-    if date_range.shape[0] > 100:
-        st.write(f"Muitos dados para exibir ({date_range.shape[0]} pontos). Por favor, selecione um intervalo menor ou uma granularidade mais alta.")
-        st.stop()
-
+with st.expander("Filtros Adicionais", expanded=False):
     # Create two columns for radio buttons (side by side)
     col3, col4 = st.columns(2)
     with col3:
@@ -106,6 +114,15 @@ with st.expander("Movimentação de Contentores", expanded=False):
     if in_out == "All":
         in_out = None
 
+with st.expander("Movimentação de Contentores", expanded=False):
+    date_range = pd.date_range(start=start, end=end, freq=gran_freq_dict[granularidade], inclusive='both')
+    
+
+    if date_range.shape[0] > 100:
+        st.write(f"Muitos dados para exibir ({date_range.shape[0]} pontos). Por favor, selecione um intervalo menor ou uma granularidade mais alta.")
+        st.stop()
+
+
     display_data, filtered_data = calculate_movement_counts(data, start, end, date_range, mean_of_transport, iso_type, in_out)
 
     if display_data.empty:
@@ -119,6 +136,10 @@ with st.expander("Movimentação de Contentores", expanded=False):
     discriminate_in_out = False
     if in_out == None:
         discriminate_in_out = st.checkbox("Discriminate In/Out", value=False)
+    
+    if granularidade == "Hora": periods = [alt.Tooltip('Period Start:T', title='Início do Período', format="%d/%m/%Y %H:%M"), 
+                                           alt.Tooltip('Period End:T', title='Fim do Período', format="%d/%m/%Y %H:%M")]
+    else: periods = [alt.Tooltip('Period Start:T', title='Início do Período'), alt.Tooltip('Period End:T', title='Fim do Período')]
 
     # Build the chart based on selection
     if chart_type == "Bar Chart":
@@ -133,11 +154,9 @@ with st.expander("Movimentação de Contentores", expanded=False):
                 xOffset="In/Out:N",
                 color=alt.Color('In/Out:N', scale=alt.Scale(domain=['In', 'Out'], range=['green', 'red'])),
                 tooltip=[
-                    alt.Tooltip('Period Start:T', title='Início do Período'),
-                    alt.Tooltip('Period End:T', title='Fim do Período'),
                     alt.Tooltip('Count:Q', title='Quantidade Total'),
                     alt.Tooltip('Date:T', title='Data')
-                ]
+                ] + periods
             ).add_params(bar_select).properties(title='Movimentação de Contentores')
         else:
             # Standard bar chart
@@ -145,47 +164,40 @@ with st.expander("Movimentação de Contentores", expanded=False):
                 x=alt.X('Date:T', title='Data', axis=alt.Axis(format=gran_format_dict[granularidade])),
                 y=alt.Y('Count:Q', title='Quantidade de Contentores'),
                 tooltip=[
-                    alt.Tooltip('Period Start:T', title='Início do Período'),
-                    alt.Tooltip('Period End:T', title='Fim do Período'),
                     alt.Tooltip('Count:Q', title='Quantidade Total'),
                     alt.Tooltip('In:Q', title='Entradas'),
                     alt.Tooltip('Out:Q', title='Saídas')
-                ]
+                ] + periods
             ).add_params(bar_select).properties(title='Movimentação de Contentores')
     else:
         # Line chart
         if discriminate_in_out:
             # Create two lines for In and Out using the "In" and "Out" columns
-            chart = alt.Chart(display_data).mark_line().encode(
+            chart = alt.Chart(display_data).mark_line(strokeDash=[5, 5]).encode(
                 x=alt.X('Date:T', title='Data', axis=alt.Axis(format=gran_format_dict[granularidade])),
                 y=alt.Y('value:Q', title='Quantidade de Contentores'),
                 color=alt.Color('variable:N', scale=alt.Scale(domain=['In', 'Out'], range=['green', 'red'])),
                 tooltip=[
-                    alt.Tooltip('Period Start:T', title='Início do Período'),
-                    alt.Tooltip('Period End:T', title='Fim do Período'),
                     alt.Tooltip('value:Q', title='Quantidade'),
-                ]
+                ] + periods
             ).transform_fold(
                 ['In', 'Out'],
                 as_=['variable', 'value']
             ).add_params(brush).properties(title='Movimentação de Contentores')
         else:
             # Standard line chart
-            chart = alt.Chart(display_data).mark_line().encode(
+            chart = alt.Chart(display_data).mark_line(strokeDash=[5, 5]).encode(
                 x=alt.X('Date:T', title='Data', axis=alt.Axis(format=gran_format_dict[granularidade])),
                 y=alt.Y('Count:Q', title='Quantidade de Contentores'),
                 tooltip=[
-                    alt.Tooltip('Period Start:T', title='Início do Período'),
-                    alt.Tooltip('Period End:T', title='Fim do Período'),
                     alt.Tooltip('Count:Q', title='Quantidade Total'),
                     alt.Tooltip('In:Q', title='Entradas'),
                     alt.Tooltip('Out:Q', title='Saídas')
-                ]
+                ] + periods
             ).add_params(brush).properties(title='Movimentação de Contentores')
 
     # Display the chart
     selection_dict = st.altair_chart(chart, use_container_width=True, on_select="rerun")
-    print(selection_dict)
     
     table_filtered_data = filtered_data.copy()
     
